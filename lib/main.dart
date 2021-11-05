cc780a7a35c264b8461a45643e6818fc86732d1f
dongkee.lee <eastflag@gmail.com>
2021-11-05 19:01:33 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screen/home_screen.dart';
+import 'package:netflix_clone/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EastflagFlex',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        // primaryColor: Colors.white60,
+        // accentColor: Colors.grey
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              Container(child: const Center(child: Text('검색'))),
+              Container(child: const Center(child: Text('목록'))),
+              Container(child: const Center(child: Text('리스트'))),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      )
+    );
+  }
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        children: <Widget>[
+          Image.asset(
+            'images/netflix_logo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          )
+        ],
+      ),
+    );
+  }
+}
